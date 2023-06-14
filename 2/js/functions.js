717f1a5afffdb91fbf315d16de25a0da7c9e3d41
Keks <keks@htmlacademy.ru>
2023-06-14 18:32:46 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,17 @@
+function checkStringLength(string, length) {
+  return string.length <= length;
+}
+
+function isPalindrome(string) {
+  const normalizedString = string.toLowerCase().replaceAll(' ', '');
+  const reversedString = normalizedString.split('').reverse().join(''); // v. 2
+  // const reversedString = [...normalizedString].reverse().join(''); // v. 1 -slowerпше
+
+  return normalizedString === reversedString;
+}
+
+function getNumsgitFromStr(text) {
+  const result = typeof(text) === 'number' ? String(text) : text.replaceAll(' ', '');
+
+  return result.replace(/\D/g, '') || NaN;
+}
