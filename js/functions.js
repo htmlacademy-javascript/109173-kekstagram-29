7cdb42d2a66969cefafe9214ce38d9fa9436dc78
Dmitriy Nosikov <deimon1993rf2@yandex.ru>
2023-06-16 17:03:18 +0300

    Начало выполнения задания 5.16. Создание функции получения часов из минут

--- js/functions.js
+++ js/functions.js
@@ -22,3 +22,32 @@ function getNumFromStr(text) {
 
   return parseInt(result, 10) || NaN;
 }
+
+// Получение часов и минут из значения минут.
+function getHoursFromMinutes(minutes) {
+  if (!minutes) {
+    return null;
+  }
+
+  const hoursMinutes = {h: 0, m: minutes};
+
+  /*
+    hoursMinutes.m = hours % 1 * 60;
+    Получаем дробную часть. т.к. она дробная,
+    то при умножении на целое число, в нашем случае - 60 минут,
+    мы получим часть от этого числа, т.е. оставшееся количество минут.
+  */
+  if (minutes >= 60) {
+    let hours = minutes / 60;
+
+    hoursMinutes.h = parseInt(hours, 10);
+    hoursMinutes.m = hours % 1 * 60;
+  }
+
+  return hoursMinutes;
+}
+
+// 5.16 Функции возвращаются
+function checkMeetingTimings(workDayStartTime, workDayEndTime, meetingStartTime, meetingDuration) {
+
+}
