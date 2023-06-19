34b9f6189315157b39c7786ebb865afb3028697d
Dmitriy Nosikov <deimon1993rf2@yandex.ru>
2023-06-19 13:04:08 +0300

    Добавление функции проверки длительности встречи в пределах рабочего дня

--- js/functions.js
+++ js/functions.js
@@ -24,19 +24,13 @@ function getNumFromStr(text) {
 }
 
 // Получение часов и минут из значения минут.
-function getHoursFromMinutes(minutes) {
+function minutesToHours(minutes) {
   if (!minutes) {
     return null;
   }
 
   const hoursMinutes = {h: 0, m: minutes};
 
-  /*
-    hoursMinutes.m = hours % 1 * 60;
-    Получаем дробную часть. т.к. она дробная,
-    то при умножении на целое число, в нашем случае - 60 минут,
-    мы получим часть от этого числа, т.е. оставшееся количество минут.
-  */
   if (minutes >= 60) {
     let hours = minutes / 60;
 
@@ -47,7 +41,19 @@ function getHoursFromMinutes(minutes) {
   return hoursMinutes;
 }
 
-// 5.16 Функции возвращаются
-function checkMeetingTimings(workDayStartTime, workDayEndTime, meetingStartTime, meetingDuration) {
+// Получение минут из строки с часами и минутами (формат - 08:00)
+function getMinutesFromTime(timeString) {
+  const normilizedTimeString = timeString.replace(/[^:|\d]/gi, '');
+  const [hours, minutes] = normilizedTimeString.split(':');
 
+  return parseInt(hours, 10) * 60 + parseInt(minutes, 10);
+}
+
+// Функция для проверки длительности встречи в рамках рабочего дня (один и тот же день)
+function checkMeetTimeInBounds(workDayStartTime, workDayEndTime, meetingStartTime, meetingDuration) {
+  // Преобразуем строковое время в минуты, для удобства манипуляций и короткой записи - в те же переменные
+  meetingDuration = getMinutesFromTime(meetingStartTime) + meetingDuration;
+  workDayStartTime = getMinutesFromTime(workDayStartTime);
+  workDayEndTime = getMinutesFromTime(workDayEndTime);
+  return meetingDuration >= workDayStartTime && meetingDuration <= workDayEndTime;
 }
