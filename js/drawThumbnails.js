c100e6bb027f78a5d158473f7a878745a72e5dbe
Deimon <deimon1993rf2@yandex.ru>
2023-06-29 21:03:14 +0300

    Добавлено TODO для переработки отрисовки миниатюр

--- js/drawThumbnails.js
+++ js/drawThumbnails.js
@@ -1,5 +1,5 @@
 const thumbnailTemplate = document.querySelector('#picture').content;
-const picturesContainer = document.querySelector('.pictures.container');
+// const picturesContainer = document.querySelector('.pictures.container');
 
 function drawThumbnail({url, description, likes, comments}) {
   if (arguments.length <= 0) {
@@ -23,6 +23,12 @@ function drawThumbnail({url, description, likes, comments}) {
 
   const thumbnail = document.createDocumentFragment();
   thumbnail.append(thumbnailElem);
+
+  /* TODO: Переделать:
+    Лучше просто возвращать созданное фото, собирать их все в контейнер и аппендить в DOM
+    т.к. каждое добавление элемента в DOM - это перерисовка всего DOM и, соответсвтенно -
+    бОльшая нагрузка на рендерер
+  */
   picturesContainer.append(thumbnail);
 }
 
