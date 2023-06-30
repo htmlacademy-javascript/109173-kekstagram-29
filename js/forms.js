650c1668fadb5819cb425e17b706c3085a4dd495
Dmitriy Nosikov <deimon1993rf2@yandex.ru>
2023-06-30 13:14:34 +0300

    Реализация валидации формы загрузки изображения

--- js/forms.js
+++ js/forms.js
@@ -1,7 +1,41 @@
 import {openImgEditor} from './formsModal.js';
+import {hashTags, comment} from './validators.js';
 
-
+const imgUploadForm = document.querySelector('.img-upload__form');
 const uploadImgInput = document.querySelector('.img-upload__input');
 
-uploadImgInput.addEventListener('change', openImgEditor);
-uploadImgInput.addEventListener('click', (evt) => console.log(evt));
+uploadImgInput.addEventListener('change', (evt) => {
+  const pristine = new Pristine(imgUploadForm, {
+    classTo: 'img-upload__field-wrapper',
+    errorClass: 'img-upload__item--invalid',
+    errorTextParent: 'img-upload__field-wrapper',
+    errorTextTag: 'span',
+    errorTextClass: 'img-upload__error'
+  });
+
+  // Валидатор хэш-тегов
+  pristine.addValidator(
+    imgUploadForm.querySelector('.text__hashtags'),
+    hashTags,
+    'Неверно заполнено поле хэш-тегов'
+  );
+
+  // Валидатор комментария
+  pristine.addValidator(
+    imgUploadForm.querySelector('.text__description'),
+    comment,
+    'Слишком длинный комментарий'
+  );
+
+  imgUploadForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    // Если форма валидна - отправляем
+    if (pristine.validate()) {
+      imgUploadForm.submit();
+    }
+  });
+
+  // Открываем редактор изображения
+  openImgEditor(evt);
+});
